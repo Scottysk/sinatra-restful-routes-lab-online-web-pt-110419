93225a829f7774c0667c02deb96809bcdd2e9679
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-21 00:12:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
-  delete '/recipes/:id/delete' do
+  delete '/recipes/:id/' do
     Recipe.find(params[:id]).destroy
     redirect '/recipes'
   end
